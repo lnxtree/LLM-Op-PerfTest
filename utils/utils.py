0fa0c1da4d9c1ff5701ca76afc18cf3f31f7b784
tr <u202215573@hust.edu.cn>
2025-03-24 22:55:44 +0800

    finish the module of flash-attention; need to further test;

---
+++ utils/utils.py
@@ -0,0 +1,27 @@
+import torch
+def ensure_divisibility(numerator, denominator):
+    """Ensure that numerator is divisible by the denominator."""
+    assert numerator % denominator == 0, "{} is not divisible by {}".format(
+        numerator, denominator
+    )
+
+def divide(numerator, denominator):
+    """Ensure that numerator is divisible by the denominator and return
+    the division value."""
+    ensure_divisibility(numerator, denominator)
+    return numerator // denominator
+
+
+def cuda_timing_decorator(func):
+    def wrapper(*args, **kwargs):
+
+        start_event = torch.cuda.Event(enable_timing=True)
+        end_event = torch.cuda.Event(enable_timing=True)
+
+        start_event.record()
+        result = func(*args, **kwargs)
+        end_event.record()
+        torch.cuda.synchronize()
+
+        elapsed_time_ms = start_event.elapsed_time(end_event) * 1000  # 时间以毫秒为单位
+        return result, elapsed_time_ms
